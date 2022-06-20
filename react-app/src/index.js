087451dd51f20268d6cf2327f4598bbd7e785400
Naggayi Daphne <daphnepearl101@gmail.com>
2022-06-20 19:55:56 +0300

    nested components

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+// Nested Component  
 function Greeting(){
   return (
    <div>
-    <h1>Hello World!!</h1>
-    <ul>
-      <li>
-        <a href="#">Hello world</a>
-      </li>
-    </ul>
+    <Person/>
+    <Message/>
    </div>
   ) 
 
 }
 
+const Person =() => <h1>Hello World!!</h1>
+const Message =() => {
+  return <p>This is a message</p>
+}
 ReactDOM.render(<Greeting/>, document.getElementById('root')); 
 
