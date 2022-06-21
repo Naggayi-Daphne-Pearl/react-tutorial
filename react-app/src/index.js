4699d6c6b87ec9073207a7c95f9acd82b5e963a0
Naggayi Daphne <daphnepearl101@gmail.com>
2022-06-21 19:02:28 +0300

    adding inline onmouse event

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -45,12 +45,18 @@ const Book =({ img, title, author}) => {
   const clickHandler  =()=>{
     alert('Hello world')
   }
+  const complexExample =(author) => {
+      console.log(author)
+  }
   return (
   <article className ='book'>
-    <img src= {img} alt="book" />
+    <img src= {img} alt="book" onMouseOver={()=>{
+      console.log(title)
+    }}/>
     <h1>{author}</h1>
     <h2 onClick={()=> console.log(title)}>{ title }</h2>
     <button type="button" onClick ={clickHandler}>Reference example</button>
+    <button onClick={()=>complexExample(author)}> more complex example</button>
   </article>)
 }
  
