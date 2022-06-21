b1f40c1185735b475af7247478d432d907884fa5
Naggayi Daphne <daphnepearl101@gmail.com>
2022-06-21 16:24:48 +0300

    proper list

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -7,14 +7,22 @@ import './index.css'
 //SETUP VARS
 const books =  [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/613KCs7szvL._AC_UL210_SR195,210_.jpg',
     title: 'Ugly love',
     author: 'Coolen Hover',
   },
   {
+    id: 2, 
     img: 'https://images-na.ssl-images-amazon.com/images/I/71nCY2F3UKL._AC_UL210_SR195,210_.jpg', 
     title: 'The Seven Husbands of Evelyn Hugo: A Novel', 
     author: 'Taylor Jenkins',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL210_SR195,210_.jpg', 
+    title: 'Why a Daughter Needs a Dad:  ', 
+    author: 'Coolen Hover',
   },
 
 ]
@@ -24,14 +32,7 @@ function Booklist(){
   return (
    <section className ='booklist'>
     {books.map((book)=>{
-      const {img, title, author} = book; 
-      return (
-        <div>
-          <img src= {img} alt =''/>
-          <h3>{title}</h3>
-          <h6>{author}</h6>
-        </div>
-      )
+      return <Book key={book.id} book={book}></Book>
     })}
    </section>
   ) 
@@ -39,11 +40,13 @@ function Booklist(){
 }
 // another way to include props
 const Book =(props) => {  
+  //
+  const {img, title, author} = props.book;
   return (
   <article className ='book'>
-    <img src= {props.img} alt="book" />
-    <h1>{props.author}</h1>
-    <h2>{ props.title }</h2>
+    <img src= {img} alt="book" />
+    <h1>{author}</h1>
+    <h2>{ title }</h2>
   </article>)
 }
  
